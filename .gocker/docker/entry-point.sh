fa511ae8dc07e1105557be5545c57ad277f1a3f6
Daniel Ortega I <daniel@gatero.mx>
2018-02-25 22:14:54 -0600

    [docker]: add entrypoint

--- .gocker/docker/entry-point.sh
+++ .gocker/docker/entry-point.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-echo "HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
+
+LOCKFILE="./vendor/.lockfile"
+MANIFEST="Gopkg.toml"
+
+printf "%b" "${PURPLE_BOLD}ENTRY POINT RUNING${NO_COLOR}"
+
+if [ ! -e Gopkg.toml ]; then
+  dep init -v
+  cp $MANIFEST $LOCKFILE > /dev/null 2>&1
+  chown -R gocker:gocker {./vendor,Gopkg.*}
+fi
+
+if [ -d ./vendor ]; then
+  if ! diff $MANIFEST $LOCKFILE > /dev/null 2>&1; then
+    dep ensure
+    cp $MANIFEST $LOCKFILE > /dev/null 2>&1
+    chown -R gocker:gocker {./vendor,Gopkg.*}
+  fi
+fi
+
+refresh run
