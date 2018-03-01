110afa42c02fe86ce05794faa00bb3eb8efd4b38
Daniel Ortega I <daniel@gatero.mx>
2018-02-28 22:32:51 -0600

    Add documentation for rebuild and run

--- actions/rebuild.sh
+++ actions/rebuild.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
-function rebuild {
-  if [[ "$(docker images)" =~ $DOCKER_IMAGE_GOLANG ]]; then
-    destroy -a
-    docker rmi -f "${DOCKER_IMAGE_GOLANG}"
-    docker build -t gatero/gocker "${GOCKER_DIR}/docker"
- else
-    docker build -t gatero/gocker "${GOCKER_DIR}/docker"
+#: Title : rebuild
+#: Date : 27/feb/2017
+#: Author : Daniel Ortega @gatero <me@daniel-ortega.mx>
+#: Version : 0.0.3
+#: Description : Run the project using docker-compose
+#: Options : 
+#:    -f) use your own docker-file
+#:    -d) default docker-compose
+#:    -h|*) help option
+
+#: Type : function
+#: Description : wrapper for docker build command
+build_image() {
+  docker build -t gatero/gocker "${GOCKER_DIR}/docker"
+}
+
+#: Type : function
+#: Description : print the configuration for up module
+doc() {
+  cat $GOCKER_DIR/doc/rebuild.txt
+}
+
+#: Type : function
+#: Description : rebuild image and containers
+rebuild() {
+  if [ $# -eq 1 ]; then
+    if [ $1 == "-h" ]; then
+      docs
+    else
+      if [[ "$(docker images)" =~ $DOCKER_IMAGE_GOLANG ]]; then
+        destroy -a && build_image
+      else
+        build_image
+      fi
+    fi
+  else
+    docs
   fi
 }
 
--- actions/run.sh
+++ actions/run.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
-function run_in_api {
+#: Title : run
+#: Date : 27/feb/2017
+#: Author : Daniel Ortega @gatero <me@daniel-ortega.mx>
+#: Version : 0.0.3
+#: Description : Run the project using docker-compose
+#: Options : 
+#:    -f) use your own docker-file
+#:    -d) default docker-compose
+#:    -h|*) help option
+
+#: Type : function
+#: Description : execute any command inside the container
+run_in_api() {
   printf "%b" "${GREEN_REGULAR}${NO_COLOR}${DOCKER_CONTAINER_API}"
   docker exec "$DOCKER_CONTAINER_API" "$@"
 }
 
-function doc {
+#: Type : function
+#: Description : print the configuration for up module
+docs() {
   cat $GOCKER_DIR/doc/run.txt
 }
 
-function run {
-  while getopts ":c:" option
-  do
-    case "${option}"
-      in
-      r) run_command "${OPTARG:2}" ;;
-      *|h) doc ;;
-    esac
-  done
+#: Type : function
+#: Description : run the program depends on passed options
+run() {
+  if [ $# -eq 1 ]; then
+    if [ $1 == "-h" ]; then
+      docs
+    else
+      run_in_api "${@}"
+    fi
+  else
+    docs
+  fi
 }
 export -f run
